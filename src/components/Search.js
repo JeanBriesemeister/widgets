4f7548bfb3ec602773a2467529b1201aa8319c60
Jean Briesemeister <jeanbr86@gmail.com>
2020-09-02 22:34:46 -0300

    Added results from wikipedia

--- src/components/Search.js
+++ src/components/Search.js
@@ -20,11 +20,33 @@ const Search = () => {
             setResults(data.query.search);
         };
 
-        if (term) {
-            search();
-        }
+        const timeoutId = setTimeout(() => {
+            if (term) {
+                search();
+            }
+        }, 500)
 
     }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                </div>
+            </div>
+        )
+    });
 
     return (
         <div>
@@ -39,6 +61,9 @@ const Search = () => {
                         className="input"
                     />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     );
