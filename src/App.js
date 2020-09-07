e64ff224687d6ef46dbf6368483f48c39d999e76
Jean Briesemeister <jeanbr86@gmail.com>
2020-09-06 22:39:49 -0300

    Working with useRef

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import DropDown from './components/DropDown';
 
 const items = [
     {
@@ -17,9 +18,36 @@ const items = [
     }
 ];
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Gree',
+        value: 'gree'
+    },
+    {
+        label: 'A Shade of Blue',
+        value: 'blue'
+    },
+];
+
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropDown, setShowDropDown] = useState(true);
+
     return (
         <div>
+            <button onClick={() => setShowDropDown(!showDropDown)}>Toogle DropDown</button>
+            {showDropDown ?
+                <DropDown
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                    options={options}
+                />
+                : null
+            }
             <Accordion items={items} />
             <Search />
         </div>
