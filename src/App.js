e53ec9d7f6620b9f48fbe0015c8d1f754a8ef496
Jean Briesemeister <jeanbr86@gmail.com>
2020-09-07 10:37:33 -0300

    Created simple router

--- src/App.js
+++ src/App.js
@@ -1,55 +1,64 @@
-import React, { useState } from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
-import DropDown from './components/DropDown';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import DropDown from "./components/DropDown";
+import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
-    {
-        title: 'What is React?',
-        content: 'React is a front end javascript framework'
-    },
-    {
-        title: 'Why use React?',
-        content: 'React is a favorite JS library among engineers'
-    },
-    {
-        title: 'How do you use React?',
-        content: 'You use React by creating components'
-    }
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
 ];
 
 const options = [
-    {
-        label: 'The Color Red',
-        value: 'red'
-    },
-    {
-        label: 'The Color Gree',
-        value: 'gree'
-    },
-    {
-        label: 'A Shade of Blue',
-        value: 'blue'
-    },
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Gree",
+    value: "gree",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
 ];
 
 export default () => {
-    const [selected, setSelected] = useState(options[0]);
-    const [showDropDown, setShowDropDown] = useState(true);
+  const [selected, setSelected] = useState(options[0]);
 
-    return (
-        <div>
-            <button onClick={() => setShowDropDown(!showDropDown)}>Toogle DropDown</button>
-            {showDropDown ?
-                <DropDown
-                    selected={selected}
-                    onSelectedChange={setSelected}
-                    options={options}
-                />
-                : null
-            }
-            <Accordion items={items} />
-            <Search />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <DropDown
+          label="Select a Color"
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
 };
